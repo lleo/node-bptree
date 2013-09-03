c05e30863d6cc5595024fbff2b7d435d3be31fa2
LLeo <lleoem@gmail.com>
2013-09-03 07:20:43 -0400

    implemented choice of MemStore or TrivialStore & some output tweaks

--- examples/stress-create-walk-delete.js
+++ examples/stress-create-walk-delete.js
@@ -33,22 +33,32 @@ var keyOrder = function(s){
   return "inorder"
 }
 
+var parseStore = function(s){
+  if (s == 'triv' || s == 'trivial')
+    return 'TrivialStore'
+  if (s == 'mem' || s == 'memory')
+    return 'MemStore'
+  return 'TrivialStore'
+}
+
 cmdLine
 .version('0.0.1')
 .option('-o|--order <n>', 'order of the BpTree', parseInt, 3)
 .option('-n|--number <n>', 'Number of Key/Val pairs', parseInt, 10)
 .option('-d|--deletion [key-order]', "Order of deletion: inorder, (rev)erse, (rand)omize", keyOrder, 'inorder')
 .option('-i|--insertion [key-order]', "Order of insertion: inorder, (rev)erse, (rand)omize", keyOrder, 'inorder')
-.option('-D|--delay-delete <ms>', "Delay each delete (milliseconds)", -1, parseInt)
+.option('-D|--io-delay <ms>', "Delay each delete (milliseconds)", parseInt, -1)
 .option('--no-display-delete', "Don't display the tree after each delete")
+.option('-S|--use-store <store>', "IO Store to use '(mem)ory', '(triv)ial'", parseStore, 'TrivialStore')
 .parse(process.argv)
 
 console.log("order             = %d", cmdLine.order)
 console.log("number            = %d", cmdLine.number)
 console.log("insertion         = %s", cmdLine.insertion)
 console.log("deletion          = %s", cmdLine.deletion)
-console.log("delay-delete      = %d", cmdLine.delayDelete)
+console.log("io-delay          = %d", cmdLine.ioDelay)
 console.log("display-delete    = %j", cmdLine.displayDelete)
+console.log("use-store         = %s", cmdLine.useStore)
 //process.exit(0)
 
 var displayNode = (
@@ -66,48 +76,60 @@ var order = cmdLine.order
   , numEnts = cmdLine.number
   , insOrder = cmdLine.insertion
   , delOrder = cmdLine.deletion
-  , delayDelete = cmdLine.delayDelete
+  , ioDelay = cmdLine.ioDelay
   , displayDelete = cmdLine.displayDelete
-  //, st = new MemStore()
-  //, st = new TrivialStore()
-  //, tree = new BpTree(order, strCmp, st)
-  , tree = new BpTree(order)
-  , _key = ""
+  , useStore = cmdLine.useStore
+  , store
+  , tree
+  , tKey = ""
   , keys = []
   , kv = {}
-  , dKeys, iKeys
+  , delKeys, insKeys
+
+if (useStore == 'MemStore') {
+  store = new MemStore(ioDelay)
+}
+if (useStore == 'TrivialStore') {
+  store = new TrivialStore(ioDelay)
+}
+
+tree = new BpTree(order, strOps.cmp, store)
 
 for (var ki = 0; ki<numEnts; ki++){
-  keys.push( _key = incStr(_key) )
-  kv[_key] = ki
+  keys.push( tKey = incStr(tKey) )
+  kv[tKey] = ki
 }
 
 
-iKeys = u.clone(keys)
+insKeys = u.clone(keys)
 if (insOrder == "randomize")
-  randomize(iKeys)
+  randomize(insKeys)
 else if (insOrder == "reverse")
-  iKeys.reverse()
+  insKeys.reverse()
 
-dKeys = u.clone(keys)
+delKeys = u.clone(keys)
 if (delOrder == "randomize")
-  randomize(dKeys)
+  randomize(delKeys)
 else if (delOrder == "reverse")
-  dKeys.reverse()
+  delKeys.reverse()
 
 var buildTree =  function(scb){
       console.log("Build Tree: %d tree.put() calls", keys.length)
       console.log("----------")
       async.eachSeries(
-        iKeys
+        insKeys
       , function(k, ecb){
-          console.log("tree.put( %s, %d )", k, kv[k])
-          tree.put(k, kv[k], ecb)
-          //tree.put(iKey, i, function(err, ids){
-          //  if (err) { next(err); return }
-          //  tree.traverseInOrder(displayNode, next)
-          //  //next(null, ids)
-          //})
+          process.stdout.write(format("tree.put( %s, %d )", k, kv[k]))
+          tree.put(k, kv[k], function(err){
+            if (err) {
+              process.stdout.write(" failed.\n")
+              console.error(err)
+              ecb(err)
+              return
+            }
+            process.stdout.write(" success\n")
+            ecb()
+          })
         }
       , function(err){
           if (err) { scb(err); return }
@@ -123,36 +145,26 @@ var buildTree =  function(scb){
       console.log("-----------")
 
       async.eachSeries(
-        dKeys
+        delKeys
       , function(k, ecb){
-          console.log("tree.del(%s)", k)
+          process.stdout.write(format("tree.del(%s)", k))
 
-          if (delayDelete < 0) {
-            tree.del(k, function(err){
-              if (err) { ecb(err); return }
-              if (displayDelete)
-                tree.traverseInOrder(displayNode, function(err, res){
-                  console.log("")
-                  ecb(err, res)
-                })
-              else
-                ecb()
-            })
-          }
-          else {
-            setTimeout(function(){
-              tree.del(k, function(err){
-                if (err) { ecb(err); return }
-                if (displayDelete)
-                  tree.traverseInOrder(displayNode, function(err, res){
-                    console.log("")
-                    ecb(err, res)
-                  })
-                else
-                  ecb()
+          tree.del(k, function(err){
+            if (err) {
+              process.stdout.write(" failed.\n")
+              console.error(err)
+              ecb(err)
+              return
+            }
+            process.stdout.write(" success.\n")
+            if (displayDelete)
+              tree.traverseInOrder(displayNode, function(err, res){
+                console.log("")
+                ecb(err, res)
               })
-            }, delayDelete)
-          }
+            else
+              ecb()
+          })
         }
       , function(err){
           if (err) { scb(err); return }
@@ -190,10 +202,19 @@ async.series(
 
     //console.log("res = %j", u.flatten(res)) //results of
     //console.log("")
-    var cruft = Object.keys(tree.storage.things)
-    console.log("cruft = %j", cruft)
-    console.log("cruft.length = %d", cruft.length)
-    console.log("tree.storage.nextId = %d", tree.storage.nextId)
+
+    if (tree.storage instanceof TrivialStore) {
+      var cruft = Object.keys(tree.storage.things)
+      console.log("cruft = %j", cruft)
+      console.log("cruft.length = %d", cruft.length)
+      console.log("tree.storage.nextId = %d", tree.storage.nextId)
+    }
+    if (tree.storage instanceof MemStore) {
+      var cruft = Object.keys(tree.storage.buffers)
+      console.log("cruft = %j", cruft)
+      console.log("cruft.length = %d", cruft.length)
+      console.log("tree.storage.nextId = %d", tree.storage.nextId)
+    }
 
     console.log("Node.CNT = %d", Node.CNT)
     console.log("THE END ")
